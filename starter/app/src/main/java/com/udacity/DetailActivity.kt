d16b7661def5ebddf44aeb6ab8fe379b2b0ec53a
alielbanna <alielbana6662@gmail.com>
2023-02-08 14:23:09 +0200

    update

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,15 +1,33 @@
 package com.udacity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.loadapp.R
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
+    private var fileName = ""
+    private var status = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        okButton.setOnClickListener {
+            returnToMainActivity()
+        }
+
+        fileName = intent.getStringExtra("fileName").toString()
+        status = intent.getStringExtra("status").toString()
+        file_name.text = fileName
+        status_text.text = status
     }
 
+    private fun returnToMainActivity() {
+        val  mainActivity = Intent(this, MainActivity::class.java)
+        startActivity(mainActivity)
+    }
 }
---
+++ starter/app/src/main/java/com/udacity/NotificationUtils.kt
@@ -0,0 +1,34 @@
+package com.udacity
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.example.loadapp.R
+
+private const val NOTIFICATION_ID = 0
+
+
+// May need to pass notification Id as a arg here.
+fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context, status: String) {
+    val contentIntent = Intent(applicationContext, DetailActivity::class.java)
+    contentIntent.apply {
+        putExtra("fileName", messageBody)
+        putExtra("status", status)
+    }
+
+    val contentPendingIntent = PendingIntent.getActivity(applicationContext, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+    val action = NotificationCompat.Action.Builder(0,"Show Details",contentPendingIntent).build()
+    val notificationBuilder = NotificationCompat.Builder(applicationContext, applicationContext.getString(
+        R.string.githubRepo_notification_channel_id))
+        .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+        .setContentTitle("Download Complete")
+        .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .addAction(action)
+
+    notify(NOTIFICATION_ID, notificationBuilder.build())
+}
